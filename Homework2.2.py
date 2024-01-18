6e2faf97e7c69ca52f8d95e70c707b019c7a1fab
artemkurto <artemkurto@gmail.com>
2024-01-18 13:14:56 +0200

    Branch 2

--- Homework2.2.py
+++ Homework2.2.py
@@ -1,9 +1,7 @@
-print("Введіть перше число")
-first_number = float(input())
-print("Введіть знак операції: + - * / ")
-sign = input()
-print("Введіть друге число")
-second_number = float(input())
+first_number = float(input("Введіть перше число: "))
+sign = input("Введіть знак операції: + - * / ")
+second_number = float(input("Введіть друге число: "))
+
 if sign == "+":
     result = first_number + second_number
     print("Результат складання дорівнює:", result)
@@ -15,9 +13,9 @@ elif sign == "*":
     print("Результат множення дорівнює:", result)
 elif sign == "/":
     if second_number == 0:
-        print("На нуль деліти не можна")
+        print("На нуль ділити не можна")
     else:
         result = first_number / second_number
         print("Результат ділення дорівнює:", result)
 else:
-    print ("Ви ввели некоректний математичний знак")
+    print("Ви ввели некоректний математичний знак")
