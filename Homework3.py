41b8befc36b839e1ebff02d6479382b524234316
artemkurto <artemkurto@gmail.com>
2024-01-21 16:37:12 +0200

    Branch 2

---
+++ Homework3.py
@@ -0,0 +1,14 @@
+products_list = input("Enter the list of products by separating them with a space: ")
+products_list = products_list.split()
+while len(products_list) > 0:
+    print("Your products list: ", products_list)
+    change_products_list = input("Enter product name if you want add it or enter product name with '-' if you want delete it: ")
+    change_products_list = change_products_list.split()
+    if change_products_list[0][0] == '-':
+        change_products_list = [item.replace('-', '') for item in change_products_list]
+        change_products_list = " ".join(change_products_list)
+        products_list.remove(change_products_list)
+    else:
+        products_list += change_products_list
+print("Your products list is empty. Program is over")
+
