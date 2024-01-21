56426b87d57e42bf6fc85aab3d08a6f6c3d2feed
artemkurto <artemkurto@gmail.com>
2024-01-21 17:44:19 +0200

    Branch 2

--- Homework3.py
+++ Homework3.py
@@ -4,13 +4,21 @@ products_list = products_list.split()
 while len(products_list) > 0:
     print("Your products list: ", products_list)
     change_products_list = ''
+
     while change_products_list == '':
-        change_products_list = input("Enter product to add it or '-product to del it: ")
+        change_products_list = input('Enter "product name" to add it or "-product name" to del it: ')
     change_products_list = change_products_list.split()
+
     if change_products_list[0][0] == '-':
-        change_products_list = [item.replace('-', '') for item in change_products_list]
+        change_products_list = [minus.replace('-', '') for minus in change_products_list]
         change_products_list = " ".join(change_products_list)
-        products_list.remove(change_products_list)
+
+        if change_products_list in products_list:
+            products_list.remove(change_products_list)
+        else:
+            print("Product isn't in the products_list")
+
     else:
         products_list += change_products_list
+
 print('Your products list is empty. Program is over')
