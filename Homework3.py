67c3b9166bcb71af4fba051e489662df9d9e2472
artemkurto <artemkurto@gmail.com>
2024-01-22 09:22:20 +0200

    Branch 2

--- Homework3.py
+++ Homework3.py
@@ -2,23 +2,21 @@ products_list = input("Enter the list of products by separating them with a spac
 products_list = products_list.split()
 
 while len(products_list) > 0:
-    print("Your products list: ", products_list)
+    print("Your products list: ", ', '.join(products_list))
     change_products_list = ''
-
     while change_products_list == '':
         change_products_list = input('Enter "product name" to add it or "-product name" to del it: ')
-    change_products_list = change_products_list.split()
 
-    if change_products_list[0][0] == '-':
-        change_products_list = [minus.replace('-', '') for minus in change_products_list]
-        change_products_list = " ".join(change_products_list)
+    if change_products_list.startswith('-'):
+        change_products_list = change_products_list.strip('-')
 
         if change_products_list in products_list:
             products_list.remove(change_products_list)
         else:
-            print("Product isn't in the products_list")
+            print(f"{change_products_list} isn't in the products_list")
 
     else:
+        change_products_list = change_products_list.split()
         products_list += change_products_list
 
 print('Your products list is empty. Program is over')
