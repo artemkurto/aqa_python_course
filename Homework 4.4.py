588b3aa6de2566ade0956660fb63c087b5f897b4
artemkurto <artemkurto@gmail.com>
2024-01-25 10:21:54 +0200

    v1

--- Homework 4.4.py
+++ Homework 4.4.py
@@ -4,7 +4,8 @@ dict_for_calculate = {}
 for word in string_for_calculate:
     if word in dict_for_calculate:
         dict_for_calculate[word] += 1
-    else: dict_for_calculate[word] = 1
+    else:
+        dict_for_calculate[word] = 1
 
 for el in dict_for_calculate:
     print((f"Символ '{el}' зустрічається {dict_for_calculate.get(el)} раз"))
