22275eae431159eedcb2eff61ed97dfadc4aea0e
artemkurto <artemkurto@gmail.com>
2024-04-08 17:22:56 +0300

    add HW18

---
+++ HW_18_data_base/tests/conftest.py
@@ -0,0 +1,33 @@
+from random import choice
+import pytest
+from HW_18_data_base.sql_user_table import SqliteUsersTable
+from HW_18_data_base.sql_products_table import SqliteProductsTable
+
+
+@pytest.fixture
+def create_delete_user() -> dict:
+    user_data = {
+        "user_id": choice(range(3, 10000)),
+        'name': 'Alex',
+        'age': 20,
+        'mail': 'Alex@mail.com',
+    }
+
+    SqliteUsersTable().create_user(**user_data)
+    yield user_data
+    SqliteUsersTable().delete_user_by_id(user_id=user_data['user_id'])
+
+
+@pytest.fixture
+def create_delete_product() -> dict:
+    product_data = {
+        "product_id": choice(range(3, 10000)),
+        'model': 'Vaio',
+        'brand': 'Sony',
+        'warehouse': 'Lviv',
+        'price': 800
+    }
+
+    SqliteProductsTable().create_product(**product_data)
+    yield product_data
+    SqliteProductsTable().delete_product_by_id(product_id=product_data['product_id'])
---
+++ HW_18_data_base/tests/test_get_users.py
@@ -0,0 +1,26 @@
+from HW_18_data_base.sql_user_table import SqliteUsersTable
+from assertpy import assert_that
+from HW_18_data_base.assertations.assertation import assert_one_user
+
+
+def test_get_all_users():
+
+    users = SqliteUsersTable().get_all_users()
+    assert_that(len(users), 'Users > 0').is_not_zero()
+    assert users
+
+
+def test_get_with_id_1():
+    user_id = 1
+
+    user = SqliteUsersTable().get_id_by_id(user_id)
+
+    assert_that(user).is_length(1)
+    user = user[0]
+
+    assert_that(user[0]).is_equal_to(user_id)
+
+
+def test_get_info_created_user(create_delete_user):
+    user = SqliteUsersTable().get_id_by_id(create_delete_user['user_id'])
+    assert_one_user(db_data=user, expected_data=create_delete_user)
